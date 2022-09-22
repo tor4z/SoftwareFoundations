4c290b801f1fa0c37ad4b8f8ef287d02744958f4
tor4z <vwenjie@hotmail.com>
2022-09-22 22:34:19 +0800

    Add makefile and compilation confifure file

--- lf/Basics.v
+++ lf/Basics.v
@@ -316,12 +316,12 @@ Module NatNumber2.
     reflexivity.
   Qed.
 
-  Notation "x + y" := (plus x y)
+  (* Notation "x + y" := (plus x y)
     (at level 50, left associativity) : nat_scope.
   Notation "x * y" := (mult x y)
     (at level 40, left associativity) : nat_scope.
   Notation "x - y" := (minus x y)
-    (at level 50, left associativity) : nat_scope.
+    (at level 50, left associativity) : nat_scope. *)
 
   Check ((0 + 1) + 1) : nat.
 
